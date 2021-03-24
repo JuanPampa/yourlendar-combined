2978c307945d5b56fa3feb0fce71757546045db4
JuanPampa <frskysard@gmail.com>
2021-03-24 13:49:52 +0000

    Hide the connection URI to use heroku process env

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 //let connectionvar = 'mongodb://127.0.0.1:27017/yourlendar';
-let connectionvar = 'mongodb+srv://juanpampa:4NzadDdEpXAQVlbr@cluster0.5c3ot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+let connectionvar = process.env.DATABASE_URI;
 // const dotenvExpand = require('dotenv-expand');
 // dotenvExpand(require('dotenv').config());
 
